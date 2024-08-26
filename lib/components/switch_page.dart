6e53aebd3db821efe1e42f22faa4d689e7715cb1
shilov6865 <shilov6865@gmail.com>
2024-08-26 16:06:22 +0000

    basic screen and assets updated

---
+++ lib/components/switch_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/view/home_screen.dart';
+import 'package:myapp/view/promt_screen.dart';
+
+class SwitchPage extends StatefulWidget {
+  const SwitchPage({super.key});
+
+  @override
+  State<SwitchPage> createState() => _SwitchPageState();
+}
+
+class _SwitchPageState extends State<SwitchPage> {
+  bool _showHomeScreen = true;
+
+  void toogleScreen() {
+    setState(() {
+      _showHomeScreen = !_showHomeScreen;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_showHomeScreen) {
+      return HomeScreen();
+    } else {
+      return PromtScreen();
+    }
+
+  }
+}
---
+++ lib/view/promt_screen.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class PromtScreen extends StatefulWidget {
+  const PromtScreen({super.key});
+
+  @override
+  State<PromtScreen> createState() => _PromtScreenState();
+}
+
+class _PromtScreenState extends State<PromtScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
